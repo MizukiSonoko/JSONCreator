d56eadfd456e622b6b9f9e63864f8bcfc9f1a5f4
mizuki.sonoko <mizuki.sonoko@gmail.com>
2016-03-21 00:37:26 +0900

    [Imp] inputから書き換えできるようにした
    evalしてるしvalidateしてないので危険

--- jsonCreator.js
+++ jsonCreator.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
 	});
 
 
- 	$(document).on("keyup","#value",change(this));
+ 	$(document).on("keyup",".value",change(this));
 
 	var res = $('.resultArea');
 	var json = null;
@@ -15,12 +15,12 @@ $(document).ready(function(){
 		return obj instanceof Object;
 	}
 
-	function printKeyValue( indent, key, value){
+	function printKeyValue( indent, key, value, route){
 	  str = '<div class="first">　　</div>';
 		for(i=0;i<indent;i++){
 			 str += '<div class="dummy">　　</div>';
 		}
-		res.append( str + '<div class="key">'+key+'</div><input type="text" class="value" id="value" value="'+value+'" >');
+		res.append( str + '<div class="key">'+key+'</div><input type="text" class="value" id="'+route+'" value="'+value+'" >');
 	}
 
 	function printIndex(indent,obj){
@@ -31,35 +31,35 @@ $(document).ready(function(){
 		res.append( str + '<div class="value" >'+obj+'</div>');
 	}
 
-	function print(indent,obj){
+	function print(indent,obj, route){
 	  str = '<div class="first">　　</div>';
 		for(i=0;i<indent;i++){
 			 str += '<div class="dummy">　　</div>';
 		}
-		res.append( str + '<input type="text" class="value" value="'+obj+'">');
+		res.append( str + '<input type="text" class="value" id="'+route+'" value="'+obj+'">');
 	}
 
-	function execution( indent, obj){
+	function execution( indent, obj, route){
 		if( obj instanceof Array){
 			$.each(obj,function(p, val){
 				if(hasChild(val)){
 					printIndex(indent, p);
-					execution(indent+1, val);			
+					execution(indent+1, val, route+"["+p+"]");			
 				}else{
-					print(indent+1,val);
+					print(indent+1,val, route);
 				}
 			});
 		}	else if(obj instanceof Object){
 			for( key in obj){
 				if(hasChild(obj[key])){
-					print(indent, key);
-					execution(indent+1,obj[key]);
+					printIndex(indent, key);
+					execution(indent+1,obj[key], route+"."+key);
 				}else{
-					printKeyValue(indent, key, obj[key]);
+					printKeyValue(indent, key, obj[key], route+"."+key);
 				}
 			}
 		} else{
-			print(indent, obj);
+			print(indent, obj, route);
 		}
 	}
 
@@ -73,12 +73,14 @@ $(document).ready(function(){
 					$('.value').remove();
 					$('.key').remove();
 					$('.first').remove();
+
 					try{
 						json = JSON.parse(str);
 					}catch(e){
 						return;
 					}
-					execution( 0, json);
+
+					execution( 0, json, "");
 					$('.dummy').css({
     				float: "left", 
     				width: "75px",
@@ -101,8 +103,10 @@ $(document).ready(function(){
 
 	function change(input){
 		return function(){
-			console.log("=======");
-			console.log($(this).val());	
+			console.log($(this).attr('id'));
+			console.log(json);
+			eval("json"+$(this).attr('id') +' = "'+$(this).val()+'"');
+			$("#jsonTextArea").val(JSON.stringify(json, null, "    "));
 		}
 	}
 
